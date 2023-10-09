c8f0cd7b033b48eb8529b54995c65314414bfcb9
sabaf222 <147254027+sabaf222@users.noreply.github.com>
2023-10-09 00:02:35 -0700

    Update app.js

--- js/app.js
+++ js/app.js
@@ -64,114 +64,11 @@ searchInput.addEventListener('keypress',(event)=>{
 })
 
 
-class Person {
-    constructor(userName,password,age){
-        this.userName=userName,
-        this.password=password,
-        this.age=age
-    }
-    startWork(){
-       console.log (`programer${this.userName} start work`)
-    }
-    getAge(){
-        return this.age
-    }
-}
-let personALi= new Person('ali',12344,23)
-personALi.startWork()
-console.log(personALi);
-
-class Profesor extends Person{
-    constructor(userName,password,age,lessen,year){
-         
-        super(userName,password,age)
-        this.lessen=lessen,
-        this.year=year
 
 
 
-    }
-    startWork(){
-        console.log(super.getAge());
-     console.log(`teacher  ${this.userName} start work`) 
-    }
-}
-
-personHamed=new Profesor('hamed',8711,32,'animetion',10)
-console.log(personHamed);
-personHamed.startWork()
 
 
 
-class Car{
-    constructor(carName,year){
-        this.carName=carName,
-        this.year=year
-    }
-    getOld=(nowYear)=>{
-        return nowYear- this.year
-    }
-}
-let date=new Date
-let nowYear=  date.getFullYear()
 
-const samandCar=new Car('samand',2018)
-console.log(samandCar.getOld(nowYear));
-
-// class Mycar{
-//     constructor(carName){
-//         this.carName=carName
-//     }
-//     getName(){
-//         return this.carName
-// //     }
-// }
-
-
-// class Model extends Mycar{
-//     constructor(carName,model){
-//         super(carName)
-//         this.model=model
-//     }
-//     show(){
-//         return this.getName() + ` model is: ${this.model} ` 
-//     }
-// }
-// const newmodel= new Model('pego','tu5')
-
-// console.log(newmodel.show());
-
-
-
-// let button=document.querySelector('button').addEventListener('click',()=>{
-//     if(navigator.getBattery){
-
-//         window.navigator.getBattery()
-//         .then(BatteryInfo=>{
-            
-//             console.log(BatteryInfo)
-//             BatteryInfo.addEventListener('levelchange',()=>{
-//                 console.log('شارژ دستگاه تغییرپیدا کرد ');
-//             })
-//             BatteryInfo.addEventListener('chargingchange',()=>{
-//                 console.log('دستگاه به شارژوصل شد یا از شارژ قطع شد ');
-//         })
-        
-//         })
-//     }
-// })
-
-// let batteryIsCharging=false
-// window.navigator.getBattery().then(battery=>{
-//     batteryIsCharging=battery.charging
-//     console.log(batteryIsCharging);
-
-//     battery.addEventListener('chargingchange',()=>{
-//     batteryIsCharging=battery.charging
-
-//     })
-// })
-
-
-// console.log(navigator.language);
 
